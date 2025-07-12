dfe94a61874c746513590584c62680bab567c710
sunilkarmurgloryweb <167843127+sunilkarmurgloryweb@users.noreply.github.com>
2025-07-12 13:52:00 +0530

    Updated ReservationModal.tsx

--- src/components/ReservationModal.tsx
+++ src/components/ReservationModal.tsx
@@ -23,7 +23,9 @@ import {
   TextField,
   useTheme,
   useMediaQuery,
-  Grid
+  Grid,
+  CircularProgress,
+  Avatar
 } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
