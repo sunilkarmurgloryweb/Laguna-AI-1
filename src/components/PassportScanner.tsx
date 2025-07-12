4d0a1b2304057d642704465a84b948b73c0dd21d
sunilkarmurgloryweb <167843127+sunilkarmurgloryweb@users.noreply.github.com>
2025-07-12 13:40:26 +0530

    Updated PassportScanner.tsx

--- src/components/PassportScanner.tsx
+++ src/components/PassportScanner.tsx
@@ -9,7 +9,7 @@ import {
   useTheme,
   useMediaQuery
 } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
+import Grid from '@mui/material/Grid';
 import {
   CameraAlt,
   Scanner,
